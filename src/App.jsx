e98c656431fae188bdcf13e88834291584aba7ba
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-25 10:50:10 -0300

    css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import Header from "./components/Header/Header"
 import Post from "./components/Post"
+import './style.css'
 
 function App() {
   return (
-    <main>
-      <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-      <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-      <Post author="Marquito" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-      <Post author="Jhones" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-      <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-    </main>
+    <>
+      <Header />
+      <main>
+        <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+        <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+        <Post author="Marquito" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+        <Post author="Jhones" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+        <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+      </main>
+    </>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import style from "./Header.module.css"
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <h1>React Feed</h1>
+    </header>
+  )
+}
