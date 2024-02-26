1fa1c4e679e458c96d5a4949f3ae6d900ad69402
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-26 20:12:27 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from "./components/header/Header"
-import Post from "./components/Post"
 import styles from './App.module.css'
 import './global.css'
 import SideBar from "./components/sidebar/SideBar"
+import Post from "./components/post/Post"
 
 function App() {
   return (
@@ -11,11 +11,14 @@ function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-          <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-          <Post author="Marquito" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-          <Post author="Jhones" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-          <Post author="Citadin" content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+          <Post
+            author="Rodrigo Citadin"
+            content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          />
+          <Post
+            author="Miguelito Donatelo"
+            content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          />
         </main>
       </div>
     </>
