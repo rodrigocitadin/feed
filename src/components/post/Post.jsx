57762a5a1b75d294df638a51c91bb97f088226e6
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-26 21:10:52 -0300

    wip

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,31 @@
-export default function Post(props) {
+import styles from './Post.module.css'
+
+export default function Post() {
   return (
-    <div>
-      <h2>{props.author}</h2>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/rodrigocitadin.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Rodrigo Citadin</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+        <time datetime="">Published 1h ago</time>
+      </header>
+      <div className={styles.content}>
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        </p>
+        <p><a href="#">see more...</a></p>
+        <p><a href="#">#sla #idk #nsei</a></p>
+      </div>
+    </article>
   )
 }
